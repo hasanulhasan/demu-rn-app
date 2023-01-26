658f7213bf29a06b9e6fc87c74a13f54d5eafce3
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-01-26 23:13:18 +0600

    icon added

--- Components/Home.js
+++ Components/Home.js
@@ -9,13 +9,16 @@ export default function Home() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerText}>Demu App</Text>
-        <Text style={styles.headerText}>icon</Text>
+        <Text style={styles.headerText}>search</Text>
       </View>
       <BannerView></BannerView>
       <View style={styles.bottomBorder} />
       <UserInfo></UserInfo>
       <View style={styles.infoText}>
         <Text style={{ fontSize: 16 }}>You don't have any Arrears now.<br /> To add new Arrears click on Puls icon and fill the necessary information</Text>
+      </View>
+      <View style={styles.plusIcon}>
+        <Text style={styles.plusIconInternal}>+</Text>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -48,5 +51,18 @@ const styles = StyleSheet.create({
     marginVertical: 100,
     marginHorizontal: 60,
     textAlign: 'center',
+  },
+  plusIcon: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginEnd: 30
+  },
+  plusIconInternal: {
+    backgroundColor: 'red',
+    borderRadius: "50%",
+    padding: 10,
+    paddingHorizontal: 20,
+    fontWeight: 'bold',
+    fontSize: 30
   }
 });
